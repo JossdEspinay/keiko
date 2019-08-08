24e5eb9633d4c3ad91540e4818941a1e3d21a437
DESPINAY Josselin <>
2019-08-08 13:52:59 +0200

    fetch first pokemon

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -3,21 +3,22 @@ import React, { PureComponent } from 'react';
 import './Pokemon.style.css';
 
 export interface IPokemon {
-    name: string;
-    id: number;
-    url: string;
+  id: number;
+  name: string;
+  height?: number;
+  weight?: number;
 };
 
 class Pokemon extends PureComponent<IPokemon> {
 
   render() {
-    const {name, id, url} = this.props;
+    const { name, id } = this.props;
     return (
       <div>
         <div>{name} - {id}</div>
-        <img src={url} />
+        <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png"} />
       </div>
     )
   }
 }
-export {Pokemon};
+export { Pokemon };
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,33 @@
 import * as React from 'react';
 import { IPokemon, Pokemon } from './../../components/Pokemon';
-import Style from './Home.style';
-class Home extends React.Component {
+import { makeGetRequest } from './../../services/networking/request'
 
+import Style from './Home.style';
+
+interface IState {
+  pokemons: IPokemon[],
+}
+class Home extends React.Component<{}, IState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      pokemons: [{ name: 'Carapuce', id: 7 },
+      { name: 'Carabaffe', id: 8 },
+      { name: 'Tortank', id: 9 },]
+    }
+  }
+  componentDidMount() {
+    makeGetRequest('/pokemon').then(resp => this.setState(old => ({
+      pokemons: [...old.pokemons, resp.body[0]],
+    })));
+  }
   render(): React.ReactNode {
-    const pokemons: IPokemon[] = [
-      { name: 'Carapuce', id: 7, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" },
-      { name: 'Carabaffe', id: 8, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png" },
-      { name: 'Tortank', id: 9, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png" },
-    ];
+    const pokemons: IPokemon[] = this.state.pokemons;
     return (
       <Style.Intro>
         {pokemons.length > 0 && pokemons.map(p => (
-          <Pokemon key={p.id} name={p.name} id={p.id} url={p.url} />
+          <Pokemon key={p.id} name={p.name} id={p.id} />
         ))}
       </Style.Intro>
     );
