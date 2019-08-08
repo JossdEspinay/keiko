f217c07a49ce2b0c39428f190c4546372d438ea7
DESPINAY Josselin <>
2019-08-08 17:48:53 +0200

    adds the loader svg

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,28 +1,32 @@
 import * as React from 'react';
+import loaderSvg from './../../assets/svg/loader.svg';
+
 import { FormattedMessage } from 'react-intl';
 import { IPokemon, Pokemon } from './../../components/Pokemon';
-import { makeGetRequest } from './../../services/networking/request'
-
+import { makeGetRequest } from './../../services/networking/request';
 import Style from './Home.style';
 
 interface IState {
   pokemons: IPokemon[],
+  loading: boolean,
 }
 class Home extends React.Component<{}, IState> {
 
   constructor(props: {}) {
     super(props);
     this.state = {
+      loading: true,
       pokemons: []
     }
   }
   componentDidMount() {
     makeGetRequest('/pokemon').then(resp => this.setState(old => ({
       pokemons: [...resp.body],
+      loading: false,
     })));
   }
   render(): React.ReactNode {
-    const pokemons: IPokemon[] = this.state.pokemons;
+    const { pokemons, loading } = this.state;
     return (
       <>
         <Style.Title><FormattedMessage id="homePage.title" /></Style.Title>
@@ -30,7 +34,7 @@ class Home extends React.Component<{}, IState> {
           {pokemons.length > 0 && pokemons.map(p => (
             <Pokemon key={p.id} name={p.name} id={p.id} weight={p.weight} height={p.height} />
           ))}
-          {pokemons.length === 0 && (<div><FormattedMessage id="homePage.loading" /></div>)}
+          {loading && (<img src={loaderSvg} />)}
         </Style.Wrapper>
       </>
     );
