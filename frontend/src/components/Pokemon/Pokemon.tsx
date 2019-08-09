3cf34bdae63e057ba1358437b187e3e8ed24c798
DESPINAY Josselin <>
2019-08-09 16:19:06 +0200

    use useStateHooks

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,7 +1,8 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-
+import turnIcon from './../../assets/svg/turn-ico.svg';
 import Style from './Pokemon.style';
+
 
 
 export interface IPokemon {
@@ -11,15 +12,25 @@ export interface IPokemon {
   weight: number;
 };
 
-const Pokemon = (props: IPokemon) => (
-  <Style.Wrapper>
-    <div>{props.name}</div>
-    <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + props.id + ".png"} alt={props.name} />
-    <div><FormattedMessage id="pokemon.id" />: {props.id}</div>
-    <div><FormattedMessage id="pokemon.weight" />: {props.weight} <FormattedMessage id="pokemon.weightUnit" /></div>
-    <div><FormattedMessage id="pokemon.height" />: {props.height} <FormattedMessage id="pokemon.heightUnit" /></div>
-  </Style.Wrapper>
-)
+const Pokemon = (props: IPokemon) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Style.Wrapper>
+      {/*tslint:disable-next-line:jsx-no-lambda*/}
+      <Style.switchIcon onClick={() => setCount(count + 1)}><img src={turnIcon} /></Style.switchIcon>
+      <div>{props.name}</div>
+      <img
+        src={
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (count % 2 === 0 ? "" : "back/") + props.id + ".png"}
+        alt={props.name}
+      />
+      <div><FormattedMessage id="pokemon.id" />: {props.id}</div>
+      <div><FormattedMessage id="pokemon.weight" />: {props.weight} <FormattedMessage id="pokemon.weightUnit" /></div>
+      <div><FormattedMessage id="pokemon.height" />: {props.height} <FormattedMessage id="pokemon.heightUnit" /></div>
+    </Style.Wrapper>
+  )
+}
 
 
 export { Pokemon };
