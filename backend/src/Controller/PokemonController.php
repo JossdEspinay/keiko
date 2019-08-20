11b04ff191695f773acaaf959fae2aada37b9677
Josselin d'Espinay <josselindespinay@gmail.com>
2019-08-20 12:24:01 +0200

    git adds the getAll(), findOneById() and create() with params methods

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -1,30 +1,42 @@
 <?php
 namespace App\Controller;
 use App\Entity\Pokemon;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class PokemonController
 {
     /**
-     * @param NormalizerInterface $normalizer
-     * @var EntityManagerInterface
+     * @var NormalizerInterface
      */
     private $normalizer;
+    /**
+     * @var EntityManagerInterface
+     */
     private $entityManager;
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
     /**
      * @param NormalizerInterface    $normalizer
      * @param EntityManagerInterface $entityManager
+     * @param SerializerInterface    $serializer
      */
-    public function __construct(NormalizerInterface $normalizer, EntityManagerInterface $entityManager) {
+    public function __construct(
+        NormalizerInterface $normalizer,
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer
+    ) {
         $this->normalizer = $normalizer;
         $this->entityManager = $entityManager;
+        $this->serializer = $serializer;
     }
 
     /**
@@ -34,22 +46,43 @@ class PokemonController
      */
     public function get(): JsonResponse
     {
-        $pokemon = new Pokemon();
-        $pokemon->setHeight('12');
-        $pokemon->setWeight('34');
-        $pokemon->setName('dedo');
-
-        $pokemonSerialized = $this->normalizer->normalize($pokemon, 'json');
-        return new JsonResponse($pokemonSerialized);
+        $pokemonRepository = $this->entityManager->getRepository(Pokemon::class);
+        $pokemons = $pokemonRepository->findAll();
+        $response = $this->normalizer->normalize($pokemons, 'json');
+        return new JsonResponse($response);
     }
 
     /**
-     * @Route("/pokemon", methods={"POST"})
-     * @return Response
+     * @Route("/pokemon/{pokemonId}", methods={"GET"})
+     * @param $pokemonId
+     * @return JsonResponse
+     * @throws ExceptionInterface
      */
-    public function create(): Response
+    public function findOneById($pokemonId): JsonResponse
     {
-        return new Response('Hello World test');
+        $pokemonRepository = $this->entityManager->getRepository(Pokemon::class);
+        $pokemon = $pokemonRepository->findOneBy([
+            'id' => $pokemonId,
+        ]);
+        $response = $this->normalizer->normalize($pokemon, 'json');
+        return new JsonResponse($response);
+    }
+
+
+    /**
+     * @Route("/pokemon", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ExceptionInterface
+     */
+    public function create(Request $request): Response
+    {
+
+        $pokemon = $this->serializer->deserialize($request->getContent(), Pokemon::class, 'json');
+        $this->entityManager->persist($pokemon);
+        $this->entityManager->flush();
+        $response = $this->normalizer->normalize($pokemon, 'json');
+        return new JsonResponse($response);
     }
 
 }
