95caeabb775436d082aac60fe4bf79642143d507
Josselin d'Espinay <josselindespinay@gmail.com>
2019-08-20 10:37:39 +0200

    configure my first road

---
+++ backend/src/Controller/PokemonController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class PokemonController
+{
+    /**
+     * @return Response
+     */
+    public function helloWorld(): Response
+    {
+        
+        return new Response('Hello World');
+    }
+}
