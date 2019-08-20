2cdd4f226ade5da868f11f355658d13796cfa199
Josselin d'Espinay <josselindespinay@gmail.com>
2019-08-20 10:59:34 +0200

    get my first pokemon in JSON format

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -1,14 +1,25 @@
 <?php
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Pokemon;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
 class PokemonController
 {
     /**
-     * @return Response
+     * @return JsonResponse
+     * @throws ExceptionInterface
      */
-    public function helloWorld(): Response
+    public function helloWorld(): JsonResponse
     {
-        
-        return new Response('Hello World');
+        $pokemon = new Pokemon();
+        $pokemon->setHeight('12');
+        $pokemon->setWeight('34');
+        $pokemon->setName('dede');
+
+        $normalizer = new ObjectNormalizer();
+        $pokemonSerialized = $normalizer->normalize($pokemon, 'json');
+        return new JsonResponse($pokemonSerialized);
     }
 }
