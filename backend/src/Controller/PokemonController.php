85a608b4e0dc97ee035d2f68390bfec4d5d5c49c
Josselin d'Espinay <josselindespinay@gmail.com>
2019-08-20 11:36:11 +0200

    add entity manager and type

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -1,19 +1,30 @@
 <?php
 namespace App\Controller;
 use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PokemonController
 {
+    /**
+     * @param NormalizerInterface $normalizer
+     * @var EntityManagerInterface
+     */
     private $normalizer;
-
-    public function __construct()
-    {
-        $this->normalizer = new ObjectNormalizer();
+    private $entityManager;
+    /**
+     * @param NormalizerInterface    $normalizer
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(NormalizerInterface $normalizer, EntityManagerInterface $entityManager) {
+        $this->normalizer = $normalizer;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -40,4 +51,5 @@ class PokemonController
     {
         return new Response('Hello World test');
     }
+
 }
